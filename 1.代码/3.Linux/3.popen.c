54bdd26c22e72972dc93ab450ba1a8d1e007df21
dofo-eat <2354787023@qq.com>
2020-04-14 08:21:58 +0800

    ./ + renewal

--- 1.代码/3.Linux/3.popen.c
+++ 1.代码/3.Linux/3.popen.c
@@ -9,55 +9,77 @@
 #include<stdlib.h>
 #include<string.h>
 #include<math.h>
+#include<error.h>
+#include<unistd.h>
+#include<sys/wait.h>
 
-char buff[1024] = {0};
+pid_t *childpid = NULL;
+int maxfd = 1024;
 
-int main(int argc, char **argv) {
-    /*if(argc != 3) {
-        perror(stderr, "Usage %s \n", argv[0]);
-        return 1;
-    }*/
-    FILE *popen (const char *command, const char *type) {
-        int pfd[2];
-        pid_t pid;
-        FILE *fp;
+FILE *popen (const char *command, const char *type) {
+    int pfd[2];
+    pid_t pid;
+    FILE *fp;
 
-        if((type[0] != 'r' || type[1] != 'w')) {
-            perror("type");
-            return NULL;
-        }
-        if(pipe(pfd) < 0) {
-            perror("set pipe fail!\n");
-            return NULL;
-        }
-        if((pid == fork()) < 0) {
-            perror("fork");
-            return NULL;
-        } else if(pid  == 0) {
-            if(*type == 'r') {
-                dup2(pfd[0], buff);
-                close(pfd[0]);
-            }
-        } else {
-               dup2(pfd[1], buff);
-                close(pfd[1]);
-            }
-        }
-        if(*type == 'r') {
-            close(pfd[1]);
-            if((fp = fopen(pfd[0], type)) == NULL) {
-                perror(popen);
-                return NULL;
-            }
-        } else {
-            close(pfd[0]);
-            if((fp = fopen(pfd[1], type)) = NULL) {
-                perror(popen);
-                return NULL;
-            }
-        } 
-        return fp;
+    if((type[0] != 'r' || type[1] != 'w')) {
+        perror("type");
+        return NULL;
     }
-
-    return 0;
+    if(childpid == NULL) {
+        if((childpid = calloc(maxfd, sizeof(pid_t))) == NULL) {
+            return NULL;
+        }
+    }
+    if(pipe(pfd) < 0) {
+        perror("set pipe fail!\n");
+        return NULL;
+    }
+    if((pid == fork()) < 0) {
+        perror("fork");
+        return NULL;
+    } else if(pid  == 0) {
+        if(*type == 'r') {
+            dup2(pfd[0], STDOUT_FILENO);
+            close(pfd[0]);
+        }
+    } else {
+           dup2(pfd[1], STDIN_FILENO);
+            close(pfd[1]);
+        }
+    }
+    for(int i = 0; i < maxfd; i++) {
+        if(childpid[i] > 0) close(i); 
+    } 
+    if(*type == 'r') {
+        close(pfd[1]);
+        if((fp = fopen(pfd[0], type)) == NULL) {
+            perror(popen);
+            return NULL;
+        }
+    } else {
+        close(pfd[0]);
+        if((fp = fopen(pfd[1], type)) = NULL) {
+            perror(popen);
+            return NULL;
+        }
+    } 
+   childpid[fileno(fp)] = pid;
+   return(fp);
 }
+
+int pclose (FILE *fp) {
+    int fd, stat;
+    pid_t pid;
+    if(childpid == NULL)  {
+        return -1;
+    }
+    fd = fileno(fp);
+    if((pid = childpid[fd]) == 0) {
+        return -1;
+    }
+    childpid[fd] = 0;
+    if(fclose(fd) == EOF) {
+        return -1;
+    }
+}
+
