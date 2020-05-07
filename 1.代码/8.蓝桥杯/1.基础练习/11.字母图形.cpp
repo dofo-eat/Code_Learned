5b97e0dbd63d8ba5294c1b2aa75e201ecfbc77ac
dofo-eat <2354787023@qq.com>
2020-05-07 19:30:45 +0800

    ./ + renewal

--- 1.代码/8.蓝桥杯/1.基础练习/11.字母图形.cpp
+++ 1.代码/8.蓝桥杯/1.基础练习/11.字母图形.cpp
@@ -8,7 +8,27 @@
 #include<iostream>
 using namespace std;
 
-int main() {
+char mmap[10000][30];
 
+int main() {
+    int n, m;
+    cin >> n >> m;
+    for(int i = 0; i < m; i++) {
+        mmap[0][i] = (char)'A' + i;
+    }
+    for(int j = 0; j < n; j++) {
+        mmap[j][0] = (char)'A' + j;
+    }
+    for(int i = 1; i < n; i++) {
+        for(int j = 1; j < m; j++) {
+            mmap[i][j] = mmap[i - 1][j - 1];
+        }
+    }
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < m; j++) {
+            cout << mmap[i][j];
+        }
+        cout << endl;
+    }
     return 0;
 }
