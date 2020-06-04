8cc0cab546f7ea9fc274b63d27b33c97205b4fc0
dofo-eat <2354787023@qq.com>
2020-06-04 18:04:47 +0800

    ./ + renewal

--- 1.代码/5.项目C语言/3.实况足球/server/server1.c
+++ 1.代码/5.项目C语言/3.实况足球/server/server1.c
@@ -1,25 +1,25 @@
 /*************************************************************************
-	> File Name: server.c
+	> File Name: server1.c
 	> Author: dofo-eat
 	> Mail:2354787023@qq.com 
-	> Created Time: 2020年06月04日 星期四 15时22分53秒
+	> Created Time: 2020年06月04日 星期四 16时01分33秒
  ************************************************************************/
 #include "../common/common.h"
 #include "../common/udp_server.h"
 #include "../common/color.h"
 #include "../common/head.h"
-#include <unistd.h>
 #include "./game.h"
-
-char *conf = "./server.conf";
+#include<sys/epoll.h>
+char *conf = "server.conf";
 
 struct User *rteam;//存放两个队的用户端
 struct User *bteam;
 int data_port;
 
+//struct Map court;
+
 int main(int argc, char **argv) {
-    int opt, port = 0, listener;
-    pthread_t draw_t;
+    int opt, port = 0, listener, epoll_fd;
     /*while ((opt = getopt(argc, argv, "p:")) != -1) {
         switch (opt) {
             case 'p'://指定端口
@@ -48,23 +48,41 @@ int main(int argc, char **argv) {
     rteam = (struct User *)calloc(MAX, sizeof(struct User));
     bteam = (struct User *)calloc(MAX, sizeof(struct User));
     
+    pthread_t draw_t; 
     if ((listener = socket_create_udp(port)) < 0) {
         perror("socket_create_udp");
         exit(1);
     }
-   
     DBG(GREEN"INFO"NONE" : Server start on Port %d\n", port);
-    
     pthread_create(&draw_t, NULL, draw, NULL);//用来绘制界面
-    while(1) {
-        w_gotoxy_puts(Message, 1, 1, "111111111");
-        struct LogData lg;
-        struct sockaddr_in client;
-        socklen_t len = sizeof(client);
-        recvfrom(listener, (void *)&lg, sizeof(lg), 0,(struct sockaddr *) &client, &len);
-        char info[1024];
-        sprintf(info, "Login : %s :%d", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+    
+    epoll_fd = epoll_create(MAX * 2);
+    
+    if (epoll_fd < 0) {
+        perror("epoll_create");
+        exit(1);
+    }
+    
+    struct epoll_event ev, events[MAX * 2];
+
+    ev.events = EPOLLIN;
+    ev.data.fd = listener;
+
+    epoll_ctl(epoll_fd, EPOLL_CTL_ADD, listener, &ev);
+    struct LogData lg;
+    struct sockaddr_in client;
+    socklen_t len = sizeof(client);
+    while (1) {
+        w_gotoxy_puts(Message, 1, 1, "waiting");
+        wrefresh(Message);
+        int nfds = epoll_wait(epoll_fd, events, MAX * 2, -1);
+
+        for (int i = 0; i < nfds; i++) {
+            char info[1024] = {0};
+            recvfrom(events[i].data.fd, (void *)&lg, sizeof(lg), 0, (struct sockaddr *)&client, &len);
+        sprintf(info, "Login : %s : %d", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
         w_gotoxy_puts(Message, 1, 2, info);
+        }
     }
     return 0;
 }
